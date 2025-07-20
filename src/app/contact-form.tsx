feff1613082990aef40c79de720e3e63582d13b2
yogeshbpatil <patilyogeshkumarb@gmail.com>
2025-07-20 12:20:37 +0530

    fix: typescript error

--- src/app/contact-form.tsx
+++ src/app/contact-form.tsx
@@ -16,9 +16,11 @@ export function ContactForm() {
   return (
     <section className="px-8 py-16">
       <div className="container mx-auto mb-20 text-center">
+        {/* @ts-ignore */}
         <Typography variant="h1" color="blue-gray" className="mb-4">
           Contact Us
         </Typography>
+        {/* @ts-ignore */}
         <Typography
           variant="lead"
           className="mx-auto w-full lg:w-5/12 !text-gray-500"
@@ -37,9 +39,11 @@ export function ContactForm() {
             {/* @ts-ignore */}
             <CardBody className="grid grid-cols-1 lg:grid-cols-7 md:gap-10">
               <div className="w-full col-span-3 rounded-lg h-full py-8 p-5 md:p-16 bg-gray-900">
+                {/* @ts-ignore */}
                 <Typography variant="h4" color="white" className="mb-2">
                   Contact Information
                 </Typography>
+                {/* @ts-ignore */}
                 <Typography
                   variant="lead"
                   className="mx-auto mb-8 text-base !text-gray-500"
@@ -48,6 +52,7 @@ export function ContactForm() {
                   hours.
                 </Typography>
                 <div className="flex gap-5">
+                  {/* @ts-ignore */}
                   <PhoneIcon className="h-6 w-6 text-white" />
                   <Typography variant="h6" color="white" className="mb-2">
                     +1(424) 535 3523
@@ -55,12 +60,15 @@ export function ContactForm() {
                 </div>
                 <div className="flex my-2 gap-5">
                   <EnvelopeIcon className="h-6 w-6 text-white" />
+                  {/* @ts-ignore */}
                   <Typography variant="h6" color="white" className="mb-2">
                     hello@mail.com
                   </Typography>
                 </div>
                 <div className="flex mb-10 gap-5">
+                  {/* @ts-ignore */}
                   <TicketIcon className="h-6 w-6 text-white" />
+                  {/* @ts-ignore */}
                   <Typography variant="h6" color="white" className="mb-2">
                     Open Support Ticket
                   </Typography>
